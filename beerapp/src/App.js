097a6f063a4d0356aee6548d02e9ea58c02bb7fd
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-10-18 16:10:36 -0400

    retrieved api data

--- beerapp/src/App.js
+++ beerapp/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {Link, Route, Switch} from "react-router-dom"
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Home"
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      beers: {}
+    }
+  }
+  render () {
+    return (
+      <div>
+        <nav>
+        <Link to="/">
+          <h1>Home</h1>
+        </Link>
+        </nav>
+        <main>
+          <Switch>
+          <Route exact path="/"
+          component={Home}/>
+          {/* <Route path="/show"
+          component={ShowBeer}/> */}
+          </Switch>
+        </main>
+      </div>
+    )
+  }
+    
+ 
 }
 
 export default App;
