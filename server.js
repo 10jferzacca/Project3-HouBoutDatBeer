3242590dd79b18c68ad5095c7e4a4ca901fb591d
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-10-23 18:55:56 -0400

    jkl

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.use(parser.json())
 app.use(parser.urlencoded({extended:true}))
 app.use("/users", UserController)
 app.use("/posts", PostController)
-app.get("/",(req,res)=> {
-    res.status(200).send("connected")
-})
+// app.get("/",(req,res)=> {
+//     res.status(200).send("connected")
+// })
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 const port = process.env.PORT || 3000
 app.set("port", process.env.PORT || 3000)
 app.listen(port, () => {
